995223bbf20dd75e74f7b05bf58f992a1cdfb873
Kenz <48181402+PixelTMT@users.noreply.github.com>
2025-02-01 22:10:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 import yt_dlp
 
 app = Flask(__name__, static_folder='./Webpage', static_url_path='')
@@ -7,6 +7,10 @@ sPort = 3000
 
 @app.route('/')
 def index():
+    vID = request.args.get('v')
+    if vID is not None:
+        # insert vID into input text and get formats
+        print(vID)
     return app.send_static_file('index.html')
 
 @app.route('/formats', methods=['POST'])
@@ -41,6 +45,28 @@ def get_formats():
     
     return jsonify(formats)
 
+@app.route('/watch', methods=['GET'])
+def get_video():
+    link = request.args.get('v')
+
+    ydl_opts = {
+        'format': 'best[ext=mp4]',
+        'quiet': True,
+        'no_warnings': True,
+        'ignoreerrors': True,
+        'extractor_args': {'youtube': {'skip': ['dash', 'hls']}},
+        'outtmpl':
+        "downloads/" + '%(title)s.%(ext)s',  # formatting the file name to be VideoName.mp4
+    }
+    try:
+        # attempt to download without errors
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(link, download=False)
+            ydl.download([link])
+            return send_file("downloads/" + f"{info['title']}.{info['ext']}", as_attachment=True)
+    except:
+        # message to be printed in the case of error
+        print("Download error!")
 
 if __name__ == '__main__':
     app.run(port=sPort, debug=True)
